7da79b0ee66a1fb2088ff4ec112f1716557f327d
shibaken <shibaryu520@gmail.com>
2023-05-31 14:53:41 +0900

    8×8マスの盤面に変更

--- Reversi.java
+++ Reversi.java
@@ -1,64 +1,80 @@
 import java.awt.*;
- 
 import javax.swing.*;
- 
 import java.awt.event.*;
- 
+
 public class Reversi extends JPanel {
     static final int WIDTH = 500; // 画面サイズ（幅）
     static final int HEIGHT = 550; // 画面サイズ（高さ）
-    int lm = 50;    // 左側余白
-    int tm = 100;   // 上側余白
-    int cs = 50;    // マスのサイズ
+    int lm = 50; // 左側余白
+    int tm = 100; // 上側余白
+    int cs = 50; // マスのサイズ
     int turn = 1; // 手番（1:黒，2:白)
- 
-    int ban[] = {0,0,0,1,2,0,0,0}; // 盤面
- 
+
+    int ban[][] = {
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 1, 2, 0, 0, 0},
+        {0, 0, 0, 2, 1, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0}
+    }; // 盤面
+
     // コンストラクタ（初期化処理）
     public Reversi() {
-        setPreferredSize(new Dimension(WIDTH,HEIGHT));
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
         addMouseListener(new MouseProc());
     }
- 
+
     // 画面描画
     public void paintComponent(Graphics g) {
         // 背景
         g.setColor(Color.gray);
         g.fillRect(0, 0, WIDTH, HEIGHT);
+
         // 盤面描画
-        int y = tm;
-        for (int j = 0; j < 8; j++) {
-            int x = lm + cs * j;
-            g.setColor(new Color(0, 170, 0));
-            g.fillRect(x, y, cs, cs);
-            g.setColor(Color.black);
-            g.drawRect(x, y, cs, cs);
-            if (ban[j] != 0) {
-                if (ban[j] == 1) {
-                    g.setColor(Color.black);
-                } else {
-                    g.setColor(Color.white);
+        for (int i = 0; i < 8; i++) {
+            int x = lm + cs * i;
+            for (int j = 0; j < 8; j++) {
+                int y = tm + cs * j;
+
+                g.setColor(new Color(0, 170, 0));
+                g.fillRect(x, y, cs, cs);
+                g.setColor(Color.black);
+                g.drawRect(x, y, cs, cs);
+
+                if (ban[i][j] != 0) {
+                    if (ban[i][j] == 1) {
+                        g.setColor(Color.black);
+                    } else {
+                        g.setColor(Color.white);
+                    }
+                    g.fillOval(x + cs / 10, y + cs / 10, cs * 8 / 10, cs * 8 / 10);
                 }
-                g.fillOval(x+cs/10, y+cs/10, cs*8/10, cs*8/10);
             }
         }
     }
- 
+
     // クリックされた時の処理用のクラス
     class MouseProc extends MouseAdapter {
         public void mouseClicked(MouseEvent e) {
             int x = e.getX();
             int y = e.getY();
+
             // 盤の外側がクリックされたときは何もしないで終了
-            if (x < lm) return;
-            if (x >= lm+cs*8) return;
-            if (y < tm) return;
-            if (y >= tm+cs) return;
+            if (x < lm || x >= lm + cs * 8 || y < tm || y >= tm + cs * 8) {
+                return;
+            }
+
             // クリックされたマスを特定
             int col = (x - lm) / cs;
-            if (ban[col] == 0) {
+            int row = (y - tm) / cs;
+
+            if (ban[col][row] == 0) {
                 // 黒コマを置く
-                ban[col] = turn;
+                ban[col][row] = turn;
+
                 // 手番の変更
                 if (turn == 1) {
                     turn = 2;
@@ -66,11 +82,12 @@ public class Reversi extends JPanel {
                     turn = 1;
                 }
             }
+
             // 再描画
             repaint();
         }
     }
- 
+
     // 起動時
     public static void main(String[] args) {
         JFrame f = new JFrame();
